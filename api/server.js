babff37c19af570fc0d3eeb8e9ef5bce0601864c
Michael Orvin <morvin1212@gmail.com>
2022-01-13 16:51:01 -0700

    Data is pulling through postman

--- api/server.js
+++ api/server.js
@@ -5,5 +5,11 @@ const server = express();
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
+
+
+server.get('/', (req, res) => {
+    res.send(`<h1>It works!</h1>`)
+});
 
 module.exports = server;
