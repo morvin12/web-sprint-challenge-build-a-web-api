c7ecdba8ae6eb14cc44a7153d330113ddf85034d
Michael Orvin <morvin1212@gmail.com>
2022-01-13 21:13:36 -0700

    Added Router framework, working on the middleware now.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,48 @@
-// Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Action.get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({ err: err.message })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    res.status(200).json(req.action)
+})
+
+router.post('/', (req, res) => {
+    Action.insert(req.body)
+    .then(response => {
+        res.status(201).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({ err: err.message })
+    })
+})
+router.put('/:id', (req, res) => {
+    Action.update(req.params.id, req.body)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({ err: err.message })
+    })
+})
+router.delete('/:id', (req, res) => {
+    Action.remove(req.params.id)
+    .then(response => {
+        res.status(204).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({ err: err.message })
+    })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,62 @@
-// Write your "projects" router here!
+const express = require('express');
+const Project = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Project.get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err.message });
+    })
+})
+
+router.get('/:id', (req, res) => {
+    res.status(200).json(req.project)
+})
+
+router.post('/', (req, res) => {
+    Project.insert(req.body)
+    .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message});
+        })
+})
+
+router.put('/:id', (req, res) => {
+    Project.update(req.params.id, req.body)
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({error: err.message})
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    Project.remove(req.params.id)
+        .then(response => {
+            res.status(204).json();
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message});
+        })
+})
+
+router.get('/:id/actions', (req, res) => {
+    Project.getProjectActions(req.params.id)
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+})
+
+
+
+module.exports = router
